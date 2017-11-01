7ad5d6867eeddd7b8d63d9efce58b14caf9769a3
quangtm210395 <quangtmse04402@fpt.edu.vn>
2017-11-02 01:17:18 +0700

    a

--- apis/song/index.js
+++ apis/song/index.js
@@ -6,7 +6,7 @@ var SongController = require('./song.controller');
 var Auth = require('../auth/auth.service');
 
 router.get('/', SongController.getAll);
-router.post('/addToPlaylist', Auth.authentication, SongController.addToPlaylist);
+router.post('/addToPlaylist', Auth.authentication(), SongController.addToPlaylist);
 router.put('/removeFromPlaylist', SongController.removeFromPlaylist);
 
 module.exports = router;
